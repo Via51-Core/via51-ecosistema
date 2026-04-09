752a0987f2cc6817191b25a1156cae6384432fbf
Fredy Bazalar <fredy.bazalar@via51.org>
2026-04-09 06:37:43 -0500

    fix: unificación de Alfa/Root y eliminación de index redundante

--- via51-gamma/src/main.tsx
+++ via51-gamma/src/main.tsx
@@ -1,15 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { SystemProvider } from '@
-gamma / context / SystemContext' // Importación Beta
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <SystemProvider>
-      <App />
-    </SystemProvider>
-  </React.StrictMode>,
-)
 
